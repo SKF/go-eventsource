17a152555ec8eee3e35cf1d896600fd91cbe83f2
Erik Karlsson <erik.karlsson@knowit.se>
2018-04-17 14:11:34 +0200

    Added test for event.go

--- eventsource/event_test.go
+++ eventsource/event_test.go
@@ -1,3 +1,20 @@
 package eventsource
+import(
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var e Event = &BaseEvent{}
+
+func Test_GetAggregatedID(t *testing.T){
+	var aggId = "123"
+	var testEvent = BaseEvent{"", userId}
+	assert.Equal(t, aggId, testEvent.GetAggregateID())
+}
+
+func Test_GetUserID(t *testing.T){
+	var userId = "testUser"
+	var testEvent = BaseEvent{"", userId}
+	assert.Equal(t, userId, event.GetUserID())
+}

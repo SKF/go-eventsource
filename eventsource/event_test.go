43d47a2ec3cfc64e38a91f66b0606c7d2dbcd0af
Erik Karlsson <erik.karlsson@knowit.se>
2018-04-17 15:05:24 +0200

    Fixed lazy copy paste

--- eventsource/event_test.go
+++ eventsource/event_test.go
@@ -9,12 +9,14 @@ var e Event = &BaseEvent{}
 
 func Test_GetAggregatedID(t *testing.T){
 	var aggId = "123"
-	var testEvent = BaseEvent{"", userId}
+	var testEvent = BaseEvent{aggId, "dontcare"}
 	assert.Equal(t, aggId, testEvent.GetAggregateID())
+	assert.Equal(t, testEvent.AggregateID, testEvent.GetAggregateID())
 }
 
 func Test_GetUserID(t *testing.T){
 	var userId = "testUser"
-	var testEvent = BaseEvent{"", userId}
-	assert.Equal(t, userId, event.GetUserID())
+	var testEvent = BaseEvent{"dontcare", userId}
+	assert.Equal(t, userId, testEvent.GetUserID())
+	assert.Equal(t, testEvent.UserID, testEvent.GetUserID())
 }

dc6d2fe985245fa9c284579f1800968650229fab
Ulf Karlsson <ulf.karlsson.gbg@gmail.com>
2018-10-31 14:33:18 +0100

    Move test helpers to own file (#17)
    * Convert to UTC in go.
    Helper functions to create and delete multiple events.
    
    * Move test helpers to own file

---
+++ eventsource/stores/sqlstore/store_test_helper.go
@@ -0,0 +1,58 @@
+package sqlstore
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/SKF/go-eventsource/eventsource"
+	"github.com/SKF/go-utility/uuid"
+)
+
+func CreateTestEvents(db *sql.DB, numberOfEvents int) (result []eventsource.Record, err error) {
+	result = []eventsource.Record{}
+
+	store := New(db, "events")
+
+	for i := 0; i < numberOfEvents; i++ {
+		aggID := uuid.New()
+		userID := uuid.New()
+		event := eventsource.Record{
+			AggregateID: aggID.String(),
+			UserID:      userID.String(),
+			Type:        fmt.Sprintf("TestEvent %d", i+1),
+			Timestamp:   time.Now().UTC(),
+			Data:        []byte("hejhopp"),
+		}
+
+		if err = store.Save(event); err != nil {
+			return
+		}
+		var records []eventsource.Record
+		records, err = store.Load(aggID.String())
+		if err != nil {
+			return
+		}
+		if len(records) != 1 {
+			err = fmt.Errorf("Expected one result from store, got %d", len(records))
+			return
+		}
+		event.SequenceID = records[0].SequenceID
+		if !reflect.DeepEqual(event, records[0]) {
+			err = fmt.Errorf("Expected identical records, saved: %v  loaded: %v", event, records[0])
+			return
+		}
+		result = append(result, records[0])
+	}
+	return
+}
+func DeleteEvents(db *sql.DB, events []eventsource.Record) error {
+	for _, e := range events {
+		_, err := db.Exec("DELETE from events WHERE sequence_id = $1", e.SequenceID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}

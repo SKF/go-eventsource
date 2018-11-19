8aeb4e4deec3b95581d82e112a85200676cff69e
Jonathan Ström <jonathan.strom@gmail.com>
2018-11-19 12:59:26 +0100

    Lintfixes

--- eventsource/stores/memorystore/store_test.go
+++ eventsource/stores/memorystore/store_test.go
@@ -1,6 +1,7 @@
 package memorystore
 
 import (
+	"context"
 	"testing"
 
 	"github.com/SKF/go-eventsource/eventsource"
@@ -9,8 +10,9 @@ import (
 )
 
 func Test_SaveLoadRollback_AllInOne(t *testing.T) {
-	s := New()
-	tx, err := s.NewTransaction(nil, []eventsource.Record{
+	ctx := context.Background()
+	store := New()
+	tx, err := store.NewTransaction(ctx, []eventsource.Record{
 		{AggregateID: "A", SequenceID: "1"},
 		{AggregateID: "A", SequenceID: "2"},
 		{AggregateID: "B", SequenceID: "1"},
@@ -20,26 +22,26 @@ func Test_SaveLoadRollback_AllInOne(t *testing.T) {
 	err = tx.Commit()
 	require.NoError(t, err)
 
-	records, err := s.Load(nil, "A")
+	records, err := store.Load(ctx, "A")
 	require.NoError(t, err)
 	assert.Len(t, records, 2)
 
-	records, err = s.Load(nil, "B")
+	records, err = store.Load(ctx, "B")
 	require.NoError(t, err)
 	assert.Len(t, records, 1)
 
-	records, err = s.Load(nil, "C")
+	records, err = store.Load(ctx, "C")
 	require.Error(t, err, "Not found")
 	assert.Len(t, records, 0)
 
 	err = tx.Rollback()
 	require.NoError(t, err)
 
-	records, err = s.Load(nil, "A")
+	records, err = store.Load(ctx, "A")
 	require.Error(t, err, "Not found")
 	assert.Len(t, records, 0)
 
-	records, err = s.Load(nil, "B")
+	records, err = store.Load(ctx, "B")
 	require.Error(t, err, "Not found")
 	assert.Len(t, records, 0)
 }

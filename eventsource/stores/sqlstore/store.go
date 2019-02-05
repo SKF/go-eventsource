0d5702405ee8b9b926b3227e2a7888c7b526cd12
Johan Sandelin <johan.sandelin@skf.com>
2019-02-05 13:20:49 +0100

    Fix typo in LoadByTimestamp SQL query

--- eventsource/stores/sqlstore/store.go
+++ eventsource/stores/sqlstore/store.go
@@ -21,7 +21,7 @@ const (
 	loadAggregateSQL           = "SELECT aggregate_id, sequence_id, created_at, user_id, type, data FROM %s WHERE aggregate_id = $1 ORDER BY sequence_id ASC LIMIT %s"
 	loadBySequenceIDSQL        = "SELECT aggregate_id, sequence_id, created_at, user_id, type, data FROM %s WHERE sequence_id > $1 ORDER BY sequence_id ASC LIMIT %s"
 	loadBySequenceIDAndTypeSQL = "SELECT aggregate_id, sequence_id, created_at, user_id, type, data FROM %s WHERE sequence_id > $1 AND type = $2 ORDER BY sequence_id ASC LIMIT %s"
-	loadByTimestampSQL         = "SELECT aggregate_id, sequence_id, created_at, user_id, type, data FROM %s WHERE timestamp > $1 ORDER BY timestamp ASC LIMIT %s"
+	loadByTimestampSQL         = "SELECT aggregate_id, sequence_id, created_at, user_id, type, data FROM %s WHERE created_at > $1 ORDER BY created_at ASC LIMIT %s"
 )
 
 // New ...

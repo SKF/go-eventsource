55f7aaf2c5fd2b36af650b310c1428e574e42f40
AndreasW <andreas.westberg@skf.com>
2020-06-02 17:24:45 +0200

    travis please compile

--- eventsource/stores/sqlstore/store_test.go
+++ eventsource/stores/sqlstore/store_test.go
@@ -36,8 +36,8 @@ func TestGetRecordsForAggregate(t *testing.T) {
 	require.NoError(t, err, "Could not create table")
 
 	eventTypes := []string{"EventTypeA", "EventTypeB", "EventTypeA", "EventTypeC", "EventTypeA"}
-	events, err := createTestEventsForAggregates(db, tableName, 10, 200, eventTypes, [][]byte{[]byte("TestData")})
-	require.NoError(t, err, "Failed to create events")
+	events, err := createTestEventsForAggregate(db, tableName, 10, 200, eventTypes, [][]byte{[]byte("TestData")})
+	require.NoError(t, err, "Failed to create events for aggregate")
 	defer func() {
 		err = cleanup(db, tableName)
 		require.NoError(t, err, "Could not perform DB cleanup")

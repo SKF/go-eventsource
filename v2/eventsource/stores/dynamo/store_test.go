70ee208d72629785b7f5837d49be3625ec6d4c4c
Jesper Olofsson <jesper.olofsson@skf.com>
2023-09-22 11:55:45 +0200

    cratet new eventsource based on aws sdk v2

---
+++ v2/eventsource/stores/dynamo/store_test.go
@@ -0,0 +1,142 @@
+package dynamo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/SKF/go-eventsource/v2/eventsource"
+	"github.com/SKF/go-utility/v2/env"
+)
+
+const dynamoTableName = "Events"
+
+func Test_SaveLoadRollback_AllInOne(t *testing.T) {
+	if testing.Short() || env.GetAsString("AWS_REGION", "") == "" {
+		t.Skip("Do not run dynamodb integration test")
+	}
+
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	require.NoError(t, err)
+
+	store := New(dynamodb.NewFromConfig(cfg), dynamoTableName)
+	tx, err := store.NewTransaction(ctx, []eventsource.Record{
+		{AggregateID: "A", Timestamp: 1},
+		{AggregateID: "A", Timestamp: 2},
+		{AggregateID: "B", Timestamp: 3},
+	}...)
+	require.NoError(t, err)
+
+	err = tx.Commit()
+	require.NoError(t, err)
+
+	records, err := store.LoadByAggregate(ctx, "A")
+	require.NoError(t, err)
+	assert.Len(t, records, 2)
+
+	records, err = store.LoadByAggregate(ctx, "A", ByTimestamp("1"))
+	require.NoError(t, err)
+	assert.Len(t, records, 1)
+
+	records, err = store.LoadByAggregate(ctx, "B")
+	require.NoError(t, err)
+	assert.Len(t, records, 1)
+
+	records, err = store.LoadByAggregate(ctx, "C")
+	require.NoError(t, err)
+	assert.Len(t, records, 0)
+
+	err = tx.Rollback()
+	require.NoError(t, err)
+
+	records, err = store.LoadByAggregate(ctx, "A")
+	require.NoError(t, err)
+	assert.Len(t, records, 0)
+
+	records, err = store.LoadByAggregate(ctx, "B")
+	require.NoError(t, err)
+	assert.Len(t, records, 0)
+}
+
+func Test_LoadByAggrWithFiltering(t *testing.T) {
+	if testing.Short() || env.GetAsString("AWS_REGION", "") == "" {
+		t.Skip("Do not run integration test")
+	}
+
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	require.NoError(t, err)
+
+	store := New(dynamodb.NewFromConfig(cfg), dynamoTableName)
+
+	tx, err := store.NewTransaction(ctx, []eventsource.Record{
+		{AggregateID: "A", Timestamp: 1, Type: "TestType", SequenceID: "1a"},
+		{AggregateID: "A", Timestamp: 2, Type: "OtherType", SequenceID: "1b"},
+		{AggregateID: "B", Timestamp: 3, Type: "TestType", SequenceID: "1c"},
+	}...)
+	require.NoError(t, err)
+
+	err = tx.Commit()
+	require.NoError(t, err)
+
+	records, err := store.LoadByAggregate(ctx, "A")
+	require.NoError(t, err)
+	assert.Len(t, records, 2)
+
+	records, err = store.LoadByAggregate(ctx, "A", ByTimestamp("1"))
+	require.NoError(t, err)
+	assert.Len(t, records, 1)
+
+	records, err = store.LoadByAggregate(ctx, "A", ByType("OtherType"))
+	require.NoError(t, err)
+	assert.Len(t, records, 1)
+
+	records, err = store.LoadByAggregate(ctx, "A", BySequenceID("1a"))
+	require.NoError(t, err)
+	assert.Len(t, records, 1)
+
+	err = tx.Rollback()
+	require.NoError(t, err)
+}
+
+func Test_LoadWithFiltering(t *testing.T) {
+	if testing.Short() || env.GetAsString("AWS_REGION", "") == "" {
+		t.Skip("Do not run integration test")
+	}
+
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	require.NoError(t, err)
+
+	store := New(dynamodb.NewFromConfig(cfg), dynamoTableName)
+
+	tx, err := store.NewTransaction(ctx, []eventsource.Record{
+		{AggregateID: "A", Timestamp: 1, Type: "TestType", SequenceID: "1a"},
+		{AggregateID: "A", Timestamp: 2, Type: "OtherType", SequenceID: "1b"},
+		{AggregateID: "B", Timestamp: 3, Type: "TestType", SequenceID: "1c"},
+	}...)
+	require.NoError(t, err)
+
+	err = tx.Commit()
+	require.NoError(t, err)
+
+	records, err := store.Load(ctx, ByTimestamp("1"))
+	require.NoError(t, err)
+	assert.Len(t, records, 2)
+
+	records, err = store.Load(ctx, ByType("TestType"))
+	require.NoError(t, err)
+	assert.Len(t, records, 2)
+
+	records, err = store.Load(ctx, BySequenceID("1a"))
+	require.NoError(t, err)
+	assert.Len(t, records, 2)
+
+	err = tx.Rollback()
+	require.NoError(t, err)
+}
